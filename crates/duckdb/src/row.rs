8e6174a39ebf3cf25cfe8393f3a8a6bc50e38cfc
Elliana May <me@mause.me>
2024-06-07 22:09:28 +0800

    Update row.rs

--- crates/duckdb/src/row.rs
+++ crates/duckdb/src/row.rs
@@ -559,12 +559,7 @@ impl<'stmt> Row<'stmt> {
 
                     let value = array.value(row);
 
-                    // TODO: remove this manual conversion once arrow-rs bug is fixed
-                    let months = (value) as i32;
-                    let days = (value >> 32) as i32;
-                    let nanos = (value >> 64) as i64;
-
-                    ValueRef::Interval { months, days, nanos }
+                    ValueRef::Interval { value.months, value.days, value.nanos }
                 }
                 _ => unimplemented!("{:?}", unit),
             },
